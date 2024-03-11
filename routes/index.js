6f18aaab209fe132504a7d3c0c4adb3a898f4f95
Obi William <williamobi818@gmail.com>
2024-03-11 14:31:54 +0100

    taask 4

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
-import UsersController from '../controllers/UsersController';
+import UsersController from "../controllers/UsersController";
 const AppController = require("../controllers/AppController");
-
+const AuthController = require("../controllers/AuthController");
 const express = require("express");
 
 const router = express.Router();
 
-
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
-router.post('/users', UsersController.postNew);
+router.post("/users", UsersController.postNew);
+router.get("/connect", AuthController.getConnect);
+router.get("/disconnect", AuthController.getDisconnect);
+router.get("/users/me", UsersController.getMe);
 module.exports = router;
