8f39db4cc3ef157b3b433db0fc89896eefcff5d0
Obi William <williamobi818@gmail.com>
2024-03-11 16:57:52 +0100

    task 5

---
+++ controllers/FilesController.js
@@ -0,0 +1,87 @@
+import { ObjectID } from "mongodb";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+import redisClient from "../utils/redis";
+import dbClient from "../utils/db";
+
+class FilesController {
+  static async postUpload(req, res) {
+    const token = req.header("X-Token");
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    }
+    console.log(req.body);
+    const { name, type, parentId = 0, isPublic = false, data } = req.body;
+
+    if (!name) {
+      res.status(400).json({ error: "Missing name" });
+      return;
+    }
+    if (!type) {
+      res.status(400).json({ error: "Missing type" });
+      return;
+    }
+    if (!data && type !== "folder") {
+      res.status(400).json({ error: "Missing data" });
+      return;
+    }
+
+    const file = {
+      name,
+      type,
+      userId,
+      parentId,
+      isPublic,
+    };
+    const files = dbClient.db.collection("files");
+
+    if (parentId) {
+      const idObject = ObjectID(parentId);
+      const parentFolder = await files.findOne({ _id: idObject });
+      if (!parentFolder) {
+        res.status(400).json({ error: "Parent not found" });
+        return;
+      }
+      if (parentFolder.type !== "folder") {
+        res.status(400).json({ error: "Parent is not a folder" });
+        return;
+      }
+    }
+
+    if (type === "folder") {
+      const result = await files.insertOne(file);
+      const [{ name, _id, isPublic, userId, type, parentId }] = result.ops;
+      res.status(201).json({
+        id: _id.toString(),
+        userId,
+        name,
+        type,
+        isPublic,
+        parentId,
+      });
+      return;
+    }
+    const folderPath = process.env.FOLDER_PATH || "/tmp/files_manager";
+    await fs.promises.mkdir(folderPath, { recursive: true });
+    const filePath = `${folderPath}/${uuidv4()}`;
+    await fs.promises.writeFile(filePath, Buffer.from(data, "base64"));
+    file.localPath = filePath;
+    if (type !== "folder") {
+      const result = await files.insertOne(file);
+      const [{ name, _id, isPublic, userId, type, parentId }] = result.ops;
+      res.status(201).json({
+        id: _id.toString(),
+        userId,
+        name,
+        type,
+        isPublic,
+        parentId,
+      });
+    }
+  }
+}
+
+module.exports = FilesController;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import UsersController from "../controllers/UsersController";
 const AppController = require("../controllers/AppController");
 const AuthController = require("../controllers/AuthController");
+const FilesController = require("../controllers/FilesController");
 const express = require("express");
 
 const router = express.Router();
@@ -11,4 +12,5 @@ router.post("/users", UsersController.postNew);
 router.get("/connect", AuthController.getConnect);
 router.get("/disconnect", AuthController.getDisconnect);
 router.get("/users/me", UsersController.getMe);
+router.post("/files", FilesController.postUpload);
 module.exports = router;
